b94a65bd5c7376c840e901b14928bd09f1ffd51a
nilesmcgiver <zen.mind.pdx@gmail.com>
2017-12-27 17:06:36 -0800

    adding in models, error handling, and integrating modules in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 # from ipython import embed
 from tri_met_service import TriMetService
 from flask import Blueprint
 
-bp = Blueprint('api', __name__)
+# bp = Blueprint('api', __name__)
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models, errors
