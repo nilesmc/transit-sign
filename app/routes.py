f71faf6a115dc313ab3fd74c6d4a293891839b51
nilesmcgiver <zen.mind.pdx@gmail.com>
2017-12-28 15:32:15 -0800

    had to include db in routes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from app import app
-from flask import render_template, flash, redirect, url_for
+from app import app, db
+from flask import render_template, flash, redirect, request, url_for
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
