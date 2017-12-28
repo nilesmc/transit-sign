e04c1cf0f847c0020e623402aef941b7aa1258d5
nilesmcgiver <zen.mind.pdx@gmail.com>
2017-12-28 13:21:33 -0800

    got flask shell up and running witn new model objects

--- lcd-transit-sign.py
+++ lcd-transit-sign.py
@@ -1,7 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Stop
 
 # tri_met_service
 
-# @app.shell_context_processor
-# def make_shell_context():
-#     return {'tri_met_service': tri_met_service }
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Stops': Stop }
