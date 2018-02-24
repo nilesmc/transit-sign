c6a4c4270269fe2a040b422cf69f7e8c95056631
nilesmcgiver <zen.mind.pdx@gmail.com>
2018-02-23 18:46:03 -0800

    setting up asset pipeline

--- app/assets.py
+++ app/assets.py
@@ -7,6 +7,12 @@ bundles = {
         'js/transit-sign.js',
         output='gen/home.js'),
 
+    'common_css': Bundle(
+      'styles/layout.scss',
+        filters='sccs',
+        output='public/css/common.css'
+    )
+
 }
 
 # assets = Environment(app)
